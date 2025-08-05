9a6cc7e013f9d1d50073bb19cd25e9efd762b0c4
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-08-05 10:45:36 -0500

    add stringBits

--- src/JakeWarmup2.java
+++ src/JakeWarmup2.java
@@ -67,7 +67,7 @@ public static int countXX(String str) {
  */
 
 public static boolean doubleX(String str) {
-    if (! str.contains("x")) return false;
+    if (!str.contains("x")) return false;
 
     boolean firstXFound = false;
 
@@ -81,4 +81,24 @@ public static boolean doubleX(String str) {
         }
     }
     return false;
+}
+
+/**
+ *
+ Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+ stringBits("Hello") → "Hlo"
+ stringBits("Hi") → "H"
+ stringBits("Heeololeo") → "Hello"
+ */
+public static String stringBits(String str) {
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i++) {
+        if (i % 2 == 0) {
+            builder.append(str.charAt(i));
+        }
+    }
+    return builder.toString();
 }
