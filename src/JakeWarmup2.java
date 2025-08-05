e35a81f2bab5deef38d05833dc3fdb0671f9a387
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-08-05 11:12:24 -0500

    add strinSplosion

--- src/JakeWarmup2.java
+++ src/JakeWarmup2.java
@@ -102,3 +102,24 @@ public static String stringBits(String str) {
     }
     return builder.toString();
 }
+
+/**
+ *
+ Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+ stringSplosion("Code") → "CCoCodCode"
+ stringSplosion("abc") → "aababc"
+ stringSplosion("ab") → "aab"
+ */
+public static String stringSplosion(String str) {
+
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i++) {
+        builder.append(str, 0, i + 1);
+    }
+
+    return builder.toString();
+
+}
