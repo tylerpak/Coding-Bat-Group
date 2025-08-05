a7e93fa0476148d98d4f3e0305d88391b3d7ad3f
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-08-05 14:58:23 -0500

    Completed more Map-1 questions

--- src/Tyler.java
+++ src/Tyler.java
@@ -403,4 +403,41 @@ public class Tyler {
         return map;
     }
 
+    public static Map<String, String> mapAB(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+
+    public static Map<String, String> topping1(Map<String, String> map) {
+        map.put("bread", "butter");
+        if(map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        return map;
+    }
+
+    public static Map<String, String> topping2(Map<String, String> map) {
+        if(map.containsKey("ice cream") && !map.get("ice cream").equals("")) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if(map.containsKey("spinach") && !map.get("spinach").equals("")) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+    public static Map<String, String> topping3(Map<String, String> map) {
+        if(map.containsKey("potato") && !map.get("potato").isEmpty()) {
+            map.put("fries", map.get("potato"));
+        }
+        if(map.containsKey("salad") && !map.get("salad").isEmpty()) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+
+
+
 }
