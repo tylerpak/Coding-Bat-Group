c5193e32fcdb72d143bb8019afbc1c7002793b53
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-08-05 15:02:42 -0500

    add last2

--- src/JakeWarmup2.java
+++ src/JakeWarmup2.java
@@ -307,3 +307,30 @@ public static String altPairs(String str) {
 
     return builder.toString();
 }
+
+/**
+ *
+ Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+ last2("hixxhi") → 1
+ last2("xaxxaxaxx") → 1
+ last2("axxxaaxx") → 2
+ */
+public static int last2(String str) {
+
+    int count = 0;
+    if (str.length() <= 2) return count;
+
+    String lastTwo = str.substring(str.length() - 2);
+
+
+    for (int i = 0; i < str.length() - 2; i++) {
+        if (str.substring(i, i + 2).equals(lastTwo)) {
+            count ++;
+        }
+    }
+
+    return count;
+
+}
