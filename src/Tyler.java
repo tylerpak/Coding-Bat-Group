22851f2db0a99dfadde0f4b23b563d19bc01acf8
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-08-05 11:05:36 -0500

    Complete with functional-1

--- src/Tyler.java
+++ src/Tyler.java
@@ -300,5 +300,33 @@ public class Tyler {
         return strings;
     }
 
+    public static List<String> copies3(List<String> strings) {
+        strings.replaceAll(n -> {String b = n; n = b + b + b; return n;});
+        return strings;
+    }
 
+    public static List<String> moreY(List<String> strings) {
+        strings.replaceAll(n -> "y" + n + "y");
+        return strings;
+    }
+
+    public static List<Integer> math1(List<Integer> nums) {
+        nums.replaceAll(n -> (n + 1) * 10);
+        return nums;
+    }
+
+    public static List<Integer> rightDigit(List<Integer> nums) {
+        nums.replaceAll(n -> n % 10);
+        return nums;
+    }
+
+    public static List<String> lower(List<String> strings) {
+        strings.replaceAll(n -> n.toLowerCase());
+        return strings;
+    }
+
+    public static List<String> noX(List<String> strings) {
+        strings.replaceAll(n -> n.replaceAll("x", ""));
+        return strings;
+    }
 }
