0513a6131df3651e1b6eb83f738391b08ed63c27
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-08-06 09:11:50 -0500

    complete with map-1

--- src/Tyler.java
+++ src/Tyler.java
@@ -454,13 +454,28 @@ public class Tyler {
             if(map.containsKey("a")) {
                 map.put("b", map.get("a"));
             }
-            else {
+            else if(map.containsKey("b")){
                 map.put("a", map.get("b"));
             }
         }
         return map;
     }
 
+    public Map<String, String> mapAB4(Map<String, String> map){
+        if(map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() > map.get("b").length()) {
+                map.put("c", map.get("a"));
+            }
+            else if (map.get("a").length() < map.get("b").length()) {
+                map.put("c", map.get("b"));
+            }
+            else if (map.get("a").length() == map.get("b").length()) {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
 
 
 }
