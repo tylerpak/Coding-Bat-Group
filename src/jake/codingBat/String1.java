eb80e81be111cc7f6617186d36ce87d15ed4d196
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-08-20 13:06:10 -0500

    add string 1 exercises

--- src/jake/codingBat/String1.java
+++ src/jake/codingBat/String1.java
@@ -66,6 +66,50 @@ public class String1 {
     }
 
     static String nonStart(String a, String b) {
-        return "";
+
+        String first = a.substring(1);
+        String second = b.substring(1);
+
+        return first + second;
+    }
+
+    static String left2(String str) {
+        String firstTwo = str.substring(0,2);
+        String remainder = str.substring(2);
+
+        return remainder + firstTwo;
+    }
+
+    static String right2(String str) {
+        String lastTwo = str.substring(str.length() - 2);
+        String remainder = str.substring(0, str.length() - 2);
+
+        return lastTwo + remainder;
+    }
+
+    static String theEnd(String str, boolean front) {
+        return front ? str.substring(0,1) : str.substring(str.length() -1);
+    }
+
+    static String withoutEnd2(String str) {
+        if (str.isEmpty() || str.length() == 1) {
+            return "";
+        } else {
+            return str.substring(1, str.length() - 1);
+        }
+    }
+
+    static String middleTwo(String str) {
+        int index = str.length() / 2;
+         return str.substring(index - 1, index + 1);
+    }
+
+    static boolean endsLy(String str) {
+        if (str.length() <= 1) {
+            return false;
+        }
+
+        String lastTwo = str.substring(str.length() - 2);
+        return lastTwo.equalsIgnoreCase("ly");
     }
 }
